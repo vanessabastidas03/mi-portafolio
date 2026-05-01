b6bde8bbcb0bc86f317c3cadbd30970e2da72e28
vanessabastidas03 <Vanessa.bastidas@campusucc.edu.co>
2026-04-30 20:11:46 -0500

    Fix static production build

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,9 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: [".next/**", "out/**", "next-env.d.ts"],
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
